c75803b4345daa013109f3a0f20e93ee59d5865d
Anirudh <anirudhraj123@gmail.com>
2019-12-01 20:54:24 -0600

    added swagger-ui

--- src/main/java/com/in28minutes/unittesting/unittesting/UnitTestingApplication.java
+++ src/main/java/com/in28minutes/unittesting/unittesting/UnitTestingApplication.java
@@ -3,7 +3,10 @@ package com.in28minutes.unittesting.unittesting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class UnitTestingApplication {
 
 	public static void main(String[] args) {
